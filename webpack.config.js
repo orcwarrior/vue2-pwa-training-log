4c26835ed18c83e8400799e784d4d60aaff891da
orcwarrior <orcwarrior@o2.pl>
2016-11-21 03:01:27 +0100

    eslint + eslint loader (webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
   },
   module: {
     rules: [
+      // only lint local *.vue files
+      {
+        enforce: 'pre',
+        test: /.vue$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
