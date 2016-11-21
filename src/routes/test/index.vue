dbd61a7f702212712b6a1bf6f7aba5dad95f6285
orcwarrior <orcwarrior@o2.pl>
2016-11-21 17:03:28 +0100

    component for /test route

---
+++ src/routes/test/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>test <br/>well,
+        <router-link to="/">Go back</router-link>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data () {
+      return {
+        foo: 'bar'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
