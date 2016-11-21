096a7b0e73e24efb4390ea146caf0612ff1de1f5
orcwarrior <orcwarrior@o2.pl>
2016-11-21 17:04:49 +0100

    vue-router map: Test component resolved with require.ensure (webpack code-spliting)

--- src/router/map.js
+++ src/router/map.js
@@ -1,9 +1,14 @@
 /**
  * Created by orcwa on 21.11.2016.
  */
-import main from '../routes/main/index.vue'
+import Main from '../routes/main/index.vue'
 
+const Test = (resolve) => {
+  require.ensure(['../routes/test/index.vue'], () => {
+    resolve(require('../routes/test/index.vue'))
+  }, 'test-route')
+}
 export default [
-  { path: '/', component: main },
-  { path: '/test', component: { template: '<div>test <br/>well, <router-link to="/">Go back</router-link></div>' }}
+  {path: '/', component: Main},
+  {path: '/test', component: Test}
 ]
