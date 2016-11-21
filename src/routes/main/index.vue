6ae0ad0092f064e3bb3f2ed20d20763f06cc6cce
orcwarrior <orcwarrior@o2.pl>
2016-11-21 03:58:17 +0100

    vue-router 2.0: main component for '/' route

---
+++ src/routes/main/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        There it is, index view. ({{someVar}})
+        <br/>
+        <router-link to="/test">Goto test view</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+      data () {
+        return {
+          someVar: 'index here'
+        }
+      }
+    }
+</script>
